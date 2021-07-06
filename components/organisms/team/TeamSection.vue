e5b261aaa5df105961e85ae711d311895d660203
Florent <flo@nuxtjs.com>
2021-07-06 17:28:56 +0200

    :construction: (Teams) wip

--- components/organisms/team/TeamSection.vue
+++ components/organisms/team/TeamSection.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="py-16 d-container-content">
-    <div v-for="team in teams" :key="team.slug">
-      <TeamTitle :slug="team.slug" :name="team.name" class="text-center pb-4" />
-      <div class="text-center text-lg pb-16"><DocusContent :document="team" /></div>
-      <div class="flex justify-center">
-        <div class="flex flex-wrap justify-center pb-16">
-          <div v-for="member in team.members" :key="member.name">
-            <TeamMember :member="member" />
-          </div>
+    <h2 class="text-display-4 font-serif">
+      <Markdown use="title" unwrap="p" />
+    </h2>
+    <div class="text-center text-lg pb-16"><DocusContent :document="team.body" /></div>
+    <div class="flex justify-center">
+      <div class="flex flex-wrap justify-center pb-16">
+        <div v-for="member in team.members" :key="member.name">
+          <TeamMember :member="member" />
         </div>
       </div>
     </div>
@@ -18,19 +18,30 @@
 import { defineComponent, ref, useContext, useFetch } from '@nuxtjs/composition-api'
 
 export default defineComponent({
-  setup() {
+  props: {
+    teamName: {
+      type: String,
+      required: true
+    }
+  },
+  setup(props) {
     const { $docus, i18n } = useContext()
     const teams = ref()
+    const team = ref([])
+
+
     useFetch(async () => {
       teams.value = await $docus
-        .search('/teams', { deep: true })
+        .search(`/teams/${props.teamName}`, { deep: true })
         .where({ 'language': i18n.locale })
         .sortBy('position', 'asc')
         .fetch()
-      teams.value = teams.value.filter(team => team.name)
+
+      team.value = teams.value[0]
+      console.log(team.value)
     })
     return {
-      teams
+      team
     }
   }
 })
--- content/en/team.md
+++ content/en/team.md
@@ -11,4 +11,13 @@ description: "The NuxtJS and its ecosystem development is lead by an internation
 ::
 
 ::team-section
+teamName: framework
+---title
+[_Framework_]{.text-primary} Teams
+::
+
+::team-section
+teamName: community
+---title
+[_Community_]{.text-primary} Teams
 ::
