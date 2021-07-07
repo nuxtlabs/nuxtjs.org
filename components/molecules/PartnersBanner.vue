a43c189af22bd108fab658c351fad0404ecf5505
Florent <flo@nuxtjs.com>
2021-07-07 08:56:49 +0200

    :ok_hand: (PartnersBanner) update width calc due to addition of vercel

--- components/molecules/PartnersBanner.vue
+++ components/molecules/PartnersBanner.vue
@@ -135,7 +135,7 @@ export default defineComponent({
   @apply flex;
   -webkit-animation: scroll 30s linear infinite;
   animation: scroll 30s linear infinite;
-  width: calc(250px * 14);
+  width: calc(250px * 16);
 }
 
 .partners__slider .partners__slide {
